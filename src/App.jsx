cc36a8ce1bf1082f3bbb367e807952f2eac228b2
ezekielvincent <vincent@haabaamail.com>
2026-08-27 07:19:42 -0400

    updated insight

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { SpeedInsights } from "@vercel/speed-insights/next";
+import { SpeedInsights } from "@vercel/speed-insights/react"
 // Vercel Analytics
 import { Analytics } from "@vercel/analytics/react";
 // Vercel Analytics
